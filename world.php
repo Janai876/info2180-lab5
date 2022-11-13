fce5043b347069c4ae3d7d60eef3a7c2061f1277
Janai Anderson <janaianderson876@gmail.com>
2022-11-13 16:24:26 -0500

    Completed Exercise 3

--- world.php
+++ world.php
@@ -1,11 +1,27 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+
+if(isset($_GET['context']))
+{
+  $context=filter_var($_GET['context'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+}
+$country=filter_var($_GET['country'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+$search="countries";
+if(isset($country))
+{
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+}
+else
+{
+  $stmt = $conn->query("SELECT * FROM countries");
+}
+
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
